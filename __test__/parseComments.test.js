0b5cd4b350bc713b773364f7565e9a5cabd93371
Uday Trivedi <trivediu@gmail.com>
2018-08-10 10:51:55 -0700

    Adjusted __Test__ require Path Location

--- __test__/parseComments.test.js
+++ __test__/parseComments.test.js
@@ -1,4 +1,4 @@
-const parseComments = require('./../petersparser.js');
+const parseComments = require('../Parser/commentBlockParser.js');
 
 describe(parseComments, () => {
   it('should be true', () => expect(true).toEqual(true));
