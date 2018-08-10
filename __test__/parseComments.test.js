c8ca6d59364fd79710257192b7f85c741dbe64c7
gurkpet <gierkep@gmail.com>
2018-08-09 23:08:53 -0400

    progress on parser

---
+++ __test__/parseComments.test.js
@@ -0,0 +1,5 @@
+const parseComments = require('./../petersparser.js');
+
+describe(parseComments, () => {
+  it('should be true', () => expect(true).toEqual(true));
+})
--- petersexample.js
+++ petersexample.js
@@ -1,6 +1,20 @@
 /**
  * something something dark side
  */
-const myFunc = (message) => console.log(message);
+const myFunc = (message) => {
+  console.log(message);
+};
 
 myFunc();
+
+/**
+ * write a second description
+ * @param { string } message
+ * @description one fish two fish red fish blue fish
+ * @description hello dolly
+ */
+const myFunc2 = (message) => {
+  console.log(message);
+};
+
+myFunc2();
