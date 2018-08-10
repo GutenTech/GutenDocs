703b48e7f6ffad724406313e6583610c1153ec5f
yuqi <zyqandrewda@gmail.com>
2018-08-10 19:57:53 -0400

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/react-client/src');
-const DIST_DIR = path.join(__dirname, '/react-client/dist');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -10,15 +10,16 @@ module.exports = {
     path: DIST_DIR,
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
+    rules: [{
+      test: /\.jsx?/,
+      include: SRC_DIR,
+      exclude: /node_modules/,
+      use: {
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-        },
       },
-    ],
+      // query: {
+      //   presets: ['react', 'es2015'],
+      // },
+    }],
   },
 };
