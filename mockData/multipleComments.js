f323ee685b8802a4fd4109ff27cf4c5d7bde92e7
yuqi <zyqandrewda@gmail.com>
2018-08-10 15:56:36 -0400

    change the test folder

---
+++ mockData/multipleComments.js
@@ -0,0 +1,23 @@
+/**
+ * Sample function
+ * @constructor
+ * @param {number} a  First multiplier
+ * @param {number} b  Second multiplier
+ * @return
+ */
+
+function mul(a, b) {
+  return a * b;
+}
+
+/**
+ * Sample function
+ * @constructor
+ * @param {number} a  Numerator
+ * @param {number} b  Denominator
+ * @return
+ */
+
+function div(a, b) {
+  return a / b;
+}
