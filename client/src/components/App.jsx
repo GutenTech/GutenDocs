9b47d35da8cc231ab29965be5c477ca671f74c2a
gurkpet <gierkep@gmail.com>
2018-08-12 11:38:24 -0400

    updates to webpack and app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import Entry from './Entry.jsx';
-import('./parsedData.js').then(result => console.log(result.default.exampleData));
+import { isRegExp } from 'util';
+
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      parsedData: null
+      parsedData: null,
     };
   }
 
+  componentWillMount() {
+    import('./parsedData.js').then(result => this.setState({parsedData: result.default.exampleData }));
+  }
 
   render() {
-    return (
-      <div className="App">
-        <h1>
-          GutenTech
-        </h1>
-        <p>
-          hello
-        </p>
-          Switch Name
-      </div>
-    );
+    if(this.state.parsedData === null) {
+      return(<div></div>)
+    } else {
+      return (
+        <div className="App">
+          <h1>
+            GutenTech
+          </h1>
+          {
+            this.state.parsedData.map((func, index) => {
+              return (
+                <div key = {index}>
+                  <h2>{func.name}</h2>
+                  <h3>{func.description}</h3>
+                </div>
+                )
+            })
+          }
+        </div>
+      );
+    }
   }
 }
