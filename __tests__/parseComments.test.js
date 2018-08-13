8960a8b84fc6fb17c9c5e0550f5e34ebf8e03be9
yuqi <zyqandrewda@gmail.com>
2018-08-13 13:28:43 -0400

    In the middle of implementing yargs

--- __tests__/parseComments.test.js
+++ __tests__/parseComments.test.js
@@ -1,4 +1,4 @@
-const parseComments = require('../src/parser/commentBlockParser.js');
+const parseComments = require('../src/parser/parseComments.js');
 
 describe('parseComments', () => {
   it('should be a function', () => expect(parseComments).toBeInstanceOf(Function));
@@ -8,35 +8,44 @@ describe('parseComments', () => {
   it('should throw error passed an object with no key of comment',
     () => expect(() => parseComments([{}])).toThrowError());
   it('should throw error passed an object with no key of name',
-    () => expect(() => parseComments([{ comment: '' }])).toThrowError());
+    () => expect(() => parseComments([{
+      comment: ''
+    }])).toThrowError());
   it('should turn a array of comments into AST objects containing info about the comment',
-    () => expect(parseComments([{ comment: '* this is a description', name: 'test' }]))
-      .toEqual([{ description: 'this is a description', name: 'test', tags: [] }]));
-  it('should turn a array of comments with tags into AST objects containing info about the comment including and array of tags', () => expect(parseComments([{ comment: '* this is a description \n * @param { String } param', name: 'test' }])).toEqual([{
+    () => expect(parseComments([{
+      comment: '* this is a description',
+      name: 'test'
+    }]))
+    .toEqual([{
+      description: 'this is a description',
+      name: 'test',
+      tags: []
+    }]));
+  it('should turn a array of comments with tags into AST objects containing info about the comment including and array of tags', () => expect(parseComments([{
+    comment: '* this is a description \n * @param { String } param',
+    name: 'test'
+  }])).toEqual([{
     description: 'this is a description',
     name: 'test',
-    tags:
-      [
-        {
-          description: null,
-          name: 'param',
-          title: 'param',
-          type: {
-            name: 'String',
-            type: 'NameExpression',
-          },
-        },
-      ],
+    tags: [{
+      description: null,
+      name: 'param',
+      title: 'param',
+      type: {
+        name: 'String',
+        type: 'NameExpression',
+      },
+    }, ],
   }]));
-  it('should add any tags of description to the description key of the object', () => expect(parseComments([{ comment: '* this is a description \n * @description some more description', name: 'test' }])).toEqual([{
+  it('should add any tags of description to the description key of the object', () => expect(parseComments([{
+    comment: '* this is a description \n * @description some more description',
+    name: 'test'
+  }])).toEqual([{
     description: 'this is a description \nsome more description',
     name: 'test',
-    tags:
-      [
-        {
-          description: 'some more description',
-          title: 'description',
-        },
-      ],
+    tags: [{
+      description: 'some more description',
+      title: 'description',
+    }, ],
   }]));
 });
