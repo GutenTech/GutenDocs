36555e5bbab5157ffc6e625506617c1fcc93c861
yuqi <zyqandrewda@gmail.com>
2018-08-13 18:19:37 -0400

    Temporary commit to upstream

--- bin/cli.js
+++ bin/cli.js
@@ -14,14 +14,9 @@ var argv = require('yargs').option('all', {
   default: false
 }).argv;
 
-if (argv.all) {
-  exec('dirname `npm root`', (e, o) => {
-    const address = `${o.slice(0, -1)}/client/dist/`;
-    parseComments(extract(argv._[0]), address);
-  });
-} else {
-  exec('dirname `npm root`', (e, o) => {
-    const address = `${o.slice(0, -1)}/client/dist/0.bundle.js`;
-    parseComments(extract(argv._[0]), address);
-  });
-}
+let input = argv.all ? './' : argv._[0];
+
+exec('dirname `npm root`', (e, o) => {
+  const address = `${o.slice(0, -1)}/client/dist/0.bundle.js`;
+  parseComments(extract(input), address);
+});
--- src/parser/extract.js
+++ src/parser/extract.js
@@ -5,30 +5,49 @@ const {
   tokTypes,
 } = require('acorn');
 const fs = require('fs');
+const dir = require('node-dir');
 
-const extract = (address) => {
-  const content = fs.readFileSync(address, 'utf8');
+const extract = (path, exclude) => {
   const result = [];
-  parse(content, {
-    onComment: (b, t, s, d) => {
-      if (b && t[0] === '*') {
-        const a = {};
-        a.comment = t;
-        const p = new Parser(undefined, content, d);
-        p.nextToken();
-
-        if (p.type === tokTypes._function) {
-          a.name = parseExpressionAt(content, d).id.name;
-        } else if (p.type === tokTypes._const) {
-          a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
-        } else {
-          a.name = p.parseStatement().declarations[0].id.name;
-        }
-        result.push(a);
+  dir.readFiles(path, {
+    exclude
+  }, (err, content, fileName, next) => {
+    console.log("?");
+    if (err) {
+      if (path instanceof RegExp) {
+        console.log("regex");
       }
-    },
-  });
-  return result;
+    } else {
+      const tag = {
+        content: [],
+        fileName,
+      };
+      result.push(tag);
+      parse(content, {
+        onComment: (b, t, s, d) => {
+          if (b && t[0] === '*') {
+            const a = {};
+            a.comment = t;
+            const p = new Parser(undefined, content, d);
+            p.nextToken();
+            if (p.type === tokTypes._function) {
+              a.name = parseExpressionAt(content, d).id.name;
+            } else if (p.type === tokTypes._const) {
+              a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
+            } else {
+              a.name = p.parseStatement().declarations[0].id.name;
+            }
+            tag.content.push(a);
+          }
+        },
+      });
+      // console.log('********************');
+      next(); // next file
+    };
+  }, () => {
+    console.log(result);
+    return result;
+  })
 };
-
+extract("*.js");
 module.exports = extract;
