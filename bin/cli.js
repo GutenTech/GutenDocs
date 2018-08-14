3794b5c0d416340f6f06e200c048ec25e569b047
yuqi <zyqandrewda@gmail.com>
2018-08-13 22:49:06 -0400

    done

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const {
   exec,
 } = require('child_process');
+const {
+  argv,
+} = require('yargs').option('all', {
+  alias: 'a',
+  default: false,
+}).option('verbose', {
+  alias: 'v',
+  default: false,
+});
 const extract = require('../src/parser/extract.js');
 const parseComments = require('../src/parser/parseComments.js');
 
-var argv = require('yargs').option('all', {
-  alias: 'a',
-  default: false
-}).option('verbose', {
-  alias: 'v',
-  default: false
-}).argv;
 
-let input = argv.all ? './' : argv._[0];
+const input = argv.all ? './' : argv._[0];
 
 exec('dirname `npm root`', (e, o) => {
-  const address = `${o.slice(0, -1)}/client/dist/0.bundle.js`;
-  parseComments(extract(input), address);
+  // const address = `${o.slice(0, -1)}/client/dist/0.bundle.js`;
+  const exclude = fs.readFileSync(`${o.slice(0, -1)}/.gutenignore`, 'utf8').split('\n');
+  console.log("in cli", input, argv._);
+  extract(input, exclude);
 });
--- src/parser/extract.js
+++ src/parser/extract.js
@@ -6,48 +6,92 @@ const {
 } = require('acorn');
 const fs = require('fs');
 const dir = require('node-dir');
+const glob = require('glob');
 
 const extract = (path, exclude) => {
   const result = [];
-  dir.readFiles(path, {
-    exclude
-  }, (err, content, fileName, next) => {
-    console.log("?");
-    if (err) {
-      if (path instanceof RegExp) {
-        console.log("regex");
+  if (glob.hasMagic(path)) {
+    console.log("glob");
+    glob(path, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(files);
+        Promise.all(files.map(fileName => new Promise((resolve) => {
+          const tag = {
+            content: [],
+            fileName,
+          };
+          const content = fs.readFileSync(fileName);
+          parse(content, {
+            onComment: (b, t, s, d) => {
+              if (b && t[0] === '*') {
+                const a = {};
+                a.comment = t;
+                const p = new Parser(undefined, content, d);
+                p.nextToken();
+                if (p.type === tokTypes._function) {
+                  a.name = parseExpressionAt(content, d).id.name;
+                } else if (p.type === tokTypes._const) {
+                  a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
+                } else {
+                  a.name = p.parseStatement().declarations[0].id.name;
+                }
+                tag.content.push(a);
+              }
+            },
+          });
+          resolve(tag);
+        }))).then((x) => {
+          console.log(x);
+          return x;
+        });
       }
-    } else {
-      const tag = {
-        content: [],
-        fileName,
-      };
-      result.push(tag);
-      parse(content, {
-        onComment: (b, t, s, d) => {
-          if (b && t[0] === '*') {
-            const a = {};
-            a.comment = t;
-            const p = new Parser(undefined, content, d);
-            p.nextToken();
-            if (p.type === tokTypes._function) {
-              a.name = parseExpressionAt(content, d).id.name;
-            } else if (p.type === tokTypes._const) {
-              a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
-            } else {
-              a.name = p.parseStatement().declarations[0].id.name;
+    })
+  } else {
+    console.log("not glob");
+    console.log(path);
+    console.log(exclude);
+    dir.readFiles(path, {
+      match: /.js$/,
+      exclude,
+    }, (err, content, fileName, next) => {
+      console.log(fileName);
+      if (err) {
+        console.log(err);
+      } else {
+        const tag = {
+          content: [],
+          fileName,
+        };
+        result.push(tag);
+        parse(content, {
+          onComment: (b, t, s, d) => {
+            if (b && t[0] === '*') {
+              const a = {};
+              a.comment = t;
+              const p = new Parser(undefined, content, d);
+              p.nextToken();
+              if (p.type === tokTypes._function) {
+                a.name = parseExpressionAt(content, d).id.name;
+              } else if (p.type === tokTypes._const) {
+                a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
+              } else {
+                a.name = p.parseStatement().declarations[0].id.name;
+              }
+              tag.content.push(a);
             }
-            tag.content.push(a);
-          }
-        },
-      });
-      // console.log('********************');
-      next(); // next file
-    };
-  }, () => {
-    console.log(result);
-    return result;
-  })
+          },
+        });
+        // console.log('********************');
+        next(); // next file
+      }
+    }, () => {
+      console.log(result);
+      return result;
+    });
+  }
 };
-extract("*.js");
+// extract("**/*.js");
+
 module.exports = extract;
