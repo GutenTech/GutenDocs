bf6b644b9e1e6f615f0520cc4094540cb81759a3
yuqi <zyqandrewda@gmail.com>
2018-08-14 17:54:00 -0400

    fixed bugs in extract

--- bin/cli.js
+++ bin/cli.js
@@ -17,16 +17,13 @@ const extract = require('../src/parser/extract.js');
 const parseComments = require('../src/parser/parseComments.js');
 
 
-const input = argv.all ? './' : argv._[0];
+const input = argv.all ? ['./'] : argv._;
 
 exec('dirname `npm root`', (e, o) => {
   const address = `${o.slice(0, -1)}/client/dist/0.bundle.js`;
   const exclude = fs.readFileSync(`${o.slice(0, -1)}/.gutenignore`, 'utf8').split('\n');
-  console.log('hello world');
   console.log("in cli", input, argv._);
   extract(input, exclude).then((data) => {
     parseComments(data, address);
   });
-  //parseComments(extract(input, exclude), address);
-
 });
