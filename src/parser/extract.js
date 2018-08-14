0191325b1d83a1d63348af947801fdaa3716c894
Uday Trivedi <trivediu@gmail.com>
2018-08-14 14:59:30 -0700

    Yuqi Updated Extract

--- src/parser/extract.js
+++ src/parser/extract.js
@@ -49,7 +49,6 @@ const walk = (x) => {
             content: [],
             name: item.path,
           };
-<<<<<<< HEAD
           const content = fs.readFileSync(item.path, 'utf8');
           acornParse(content, tag.content);
           result.push(tag);
@@ -73,79 +72,5 @@ const extract = (arr, exclude) => Promise.all(arr.map(x => globParse(x))).then((
 extract(['**/*.js']).then(x => console.log(x));
 extract(['extract.js']).then(x => console.log(x));
 extract(['./']).then(x => console.log(x));
-=======
-          const content = fs.readFileSync(fileName);
-          parse(content, {
-            onComment: (b, t, s, d) => {
-              if (b && t[0] === '*') {
-                const a = {};
-                a.comment = t;
-                const p = new Parser(undefined, content, d);
-                p.nextToken();
-                if (p.type === tokTypes._function) {
-                  a.name = parseExpressionAt(content, d).id.name;
-                } else if (p.type === tokTypes._const) {
-                  a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
-                } else {
-                  a.name = p.parseStatement().declarations[0].id.name;
-                }
-                tag.content.push(a);
-              }
-            },
-          });
-          resolve(tag);
-        }))).then((x) => {
-          console.log(x);
-          resolve(x);
-        });
-      }
-    })
-  } else {
-    console.log("not glob");
-    console.log(path);
-    console.log(exclude);
-    dir.readFiles(path, {
-      match: /.js$/,
-      exclude,
-    }, (err, content, fileName, next) => {
-      console.log(fileName);
-      if (err) {
-        console.log(err);
-      } else {
-        const tag = {
-          content: [],
-          fileName,
-        };
-        result.push(tag);
-        parse(content, {
-          onComment: (b, t, s, d) => {
-            if (b && t[0] === '*') {
-              const a = {};
-              a.comment = t;
-              const p = new Parser(undefined, content, d);
-              p.nextToken();
-              if (p.type === tokTypes._function) {
-                a.name = parseExpressionAt(content, d).id.name;
-              } else if (p.type === tokTypes._const) {
-                a.name = p.parseVarStatement(p.startNode()).declarations[0].id.name;
-              } else {
-                a.name = p.parseStatement().declarations[0].id.name;
-              }
-              tag.content.push(a);
-            }
-          },
-        });
-        // console.log('********************');
-        next(); // next file
-      }
-    }, () => {
-      console.log('extract result:', result);
-      resolve(result);
-    });
-  }})
-};
-// extract("**/*.js");
-extract('./');
->>>>>>> Committed Changes before switching to P7
 
 module.exports = extract;
