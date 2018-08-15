0547b7b8659c8cd2d972c752af5fc333fc523cb8
gurkpet <gierkep@gmail.com>
2018-08-15 13:14:26 -0400

    minor fixes

--- bin/help.js
+++ bin/help.js
@@ -3,13 +3,14 @@ const helpTxt = () => {
   Usage: gutendocs <command>
   
   where <command> is one of:
-    gutendocs --init              initialize gutendocs with .gutenrc and a api htlm folder called GutenAPI
-    gutendocs --init <filename>   initialize gutendocs with .gutenrc and an api htlm file called <filename>
+    gutendocs --init, --i         initialize gutendocs with .gutenrc and a api html folder called GutenAPI
+    gutendocs --init <folder>     initialize gutendocs with .gutenrc and an api html folder called <folder>
     gutendocs <filename>          parse <filename> and add the data to the auto-generated document
-    gutendocs --all               parse all js and jsx files in and above current directory
-    gutendocs --refresh           refresh the files in the gutendocs api folder to virgin state
-    gutendocs --version           logs the current version of gutendocs
-    gutendocs --config            updates the API styles to reflect the lastest update of config
+    gutendocs --all, --a          parse all js and jsx files in and above current directory
+    gutendocs --refresh, --r      refresh the files in the gutendocs api folder to virgin state
+    gutendocs --version, --v      logs the current version of gutendocs
+    gutendocs --config, --c       updates the API styles to reflect the lastest update of config
+    gutendocs --info              get info on available commands
     `);
 };
 
