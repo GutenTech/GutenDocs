945742551fd20aa0b471d0fa2ef203ec50def6f4
mcsteezy <davidpark9224@gmail.com>
2018-08-16 12:26:05 -0700

    react html css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-import Sidebarr from './SideBar.jsx';
+// import Sidebarr from './SideBar.jsx';
+import Intro from './GettingStarted.jsx';
+import '../../dist/styles.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 // import gutenDocsLogo from '../../dist/resources/gutendocslogo.png';
 /* eslint-enable */
 const getData = () => import('./parsedData.json');
@@ -18,8 +23,7 @@ export default class App extends Component {
 
   componentDidMount() {
     getData().then(data => this.setState({ parsedData: data.default }));
-    getConfig().then(data => {console.log(data)
-      this.setState({ configData: data.default })});
+    getConfig().then(data => this.setState({ configData: data.default }));
   }
 
   render() {
@@ -27,44 +31,47 @@ export default class App extends Component {
     const { bulma } = configData;
     console.log(this.state);
     return (
-      <div className="App">
-      {console.log(parsedData)}
-        <h1>
-          {/*eslint-disable*/}
-          <img {...configData.banner} style={configData.banner ? {} : { display: 'none' }} />
-          {/* eslint-enable */}
-          <div style={configData.banner ? { display: 'none' } : {}} />
-          GutenDocs
-          <div className={bulma.example}>Hello</div>
-
-          <div />
-        </h1>
-        {
-          parsedData.map(file => (
-            <React.Fragment>
-              <h2>
-                {
-                  `Functions in the file ${path.basename(file.fileName)}`
-                }
-              </h2>
-              <div>
-                {
-                  file.content.map(func => (
-                    <div>
-                      <h2 style = {{marginLeft: 240, marginTop: 80}}>
-                        {`${func.name} function`}
-                      </h2>
-                      <h3 style ={{marginLeft: 240, marginTop: 80}}>
-                        {func.description}
-                      </h3>
-                    </div>
-                  ))
-                }
-              </div>
-            </React.Fragment>
-          ))
-      }
-      </div>
+      <Router>
+        <div className="App">
+          <h1 className="logo">
+            {/*eslint-disable*/}
+            <img {...configData.banner} style={configData.banner ? {} : { display: 'none' }} />
+            {/* eslint-enable */}
+          </h1>
+          <div className = "download">
+            <button id="npm"><i class="fa fa-download"></i> npm Download</button>
+          </div>
+          <div className = "starter">
+            <Intro/>
+          </div>
+          {
+            parsedData.map(file => (
+              <React.Fragment>
+                
+                {/* <h2>
+                  {
+                    `Functions in the file ${path.basename(file.fileName)}`
+                  }
+                </h2> */}
+                <div>
+                  {
+                    file.content.map(func => (
+                      <div className = "body">
+                        <h2>
+                          {`${func.name} function`}
+                        </h2>
+                        <p>
+                          {func.description}
+                        </p>
+                      </div>
+                    ))
+                  }
+                </div>
+              </React.Fragment>
+            ))
+        }
+        </div>
+    </Router>
     );
   }
 }
