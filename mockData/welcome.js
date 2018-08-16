02e2e5bff04c1126ed22801cb659b6b7315df60b
gurkpet <gierkep@gmail.com>
2018-08-16 16:43:04 -0400

    demo stuff

---
+++ mockData/welcome.js
@@ -0,0 +1,13 @@
+/**
+ * We are Gutentech
+ */
+const welcome = function () {
+  console.log('We are gutentech!')
+}
+
+/**
+ * @description here is a demo of our product
+ */
+const isInItGreat = function () {
+  console.log('We are gutentech!')
+}
