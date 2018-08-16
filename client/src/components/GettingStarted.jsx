5ba486a87798873ca37d400c9286edfcc8b3ba1d
mcsteezy <davidpark9224@gmail.com>
2018-08-16 12:26:05 -0700

    react html css

---
+++ client/src/components/GettingStarted.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import example from '../../dist/imgs/example.png';
+//ideally we would have separate components for each h2
+const Intro = () => {
+    return(
+       <div className = "intros">
+            <h2>Getting Started</h2>
+            <p>"Bacon ipsum dolor amet biltong ham picanha corned beef pork chop pancetta kevin porchetta short ribs leberkas tenderloin ball tip rump. Jerky ball tip boudin kielbasa venison biltong short ribs turducken sausage. Filet mignon strip steak shankle, prosciutto pork belly tongue turkey chicken. Picanha biltong pork belly tail venison kielbasa leberkas ham strip steak pork landjaeger pastrami swine. Hamburger ham hock meatball corned beef, jerky tail buffalo filet mignon bacon pork. Meatball beef turducken hamburger leberkas kielbasa burgdoggen tail ham hock picanha pastrami buffalo drumstick salami."
+            </p><br/><br/>
+            <h2>Installation</h2>
+            <p>"Jerky ball tip boudin kielbasa venison biltong short ribs turducken sausage. Filet mignon strip steak shankle, prosciutto pork belly tongue turkey chicken. Picanha biltong pork belly tail venison kielbasa leberkas ham strip steak pork landjaeger pastrami swine. Hamburger ham hock meatball corned beef, jerky tail buffalo filet mignon bacon pork. Meatball beef turducken hamburger leberkas kielbasa burgdoggen tail ham hock picanha pastrami buffalo drumstick salami."
+            </p><br/><br/>
+            <h2>Examples</h2>
+            <p>"Bacon ipsum dolor amet biltong ham picanha corned beef pork chop pancetta kevin porchetta short ribs leberkas tenderloin ball tip rump. Jerky ball tip boudin kielbasa venison biltong short ribs turducken sausage. Filet mignon strip steak shankle, prosciutto pork belly tongue turkey chicken. Picanha biltong pork belly tail venison kielbasa leberkas ham strip steak pork landjaeger pastrami swine. Hamburger ham hock meatball corned beef, jerky tail buffalo filet mignon bacon pork. 
+            </p>
+            {/* <img src={example} width="100" height="50" /> */}
+            <br/><br/>
+            <h2>Support</h2>
+            <p>"Bacon ipsum dolor amet biltong ham picanha corned beef pork chop pancetta kevin porchetta short ribs leberkas tenderloin ball tip rump. Jerky ball tip boudin kielbasa venison biltong short ribs turducken sausage. buffalo drumstick salami."
+            </p><br/><br/>
+            <h2>FAQ</h2>
+            <p>"Jerky ball tip boudin kielbasa venison biltong short ribs turducken sausage. buffalo drumstick salami."</p><hr/><br/><br/>
+       </div>
+  )
+}
+
+export default Intro;
--- client/src/components/SideHash.jsx
+++ client/src/components/SideHash.jsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
+//moved to branch p1 due to bugs
