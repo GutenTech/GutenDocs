7b59854eef7ccbf0ded139cdf1cf69c9699df9d7
Uday Trivedi <trivediu@gmail.com>
2018-08-17 08:53:53 -0700

    Import Template as Object

--- src/parser/parseComments.js
+++ src/parser/parseComments.js
@@ -1,6 +1,6 @@
 const doctrine = require('doctrine');
 const fs = require('fs');
-const wp = require('./utils/webpackTemplates.js');
+const { parseCommentsTemplate } = require('./utils/webpackTemplates.js');
 const errors = require('./utils/errors.js');
 
 /**
@@ -9,7 +9,7 @@ const errors = require('./utils/errors.js');
 
 const saveTags = (data, path) => {
   const dataToSave = JSON.stringify(data).replace(/\\n/g, '\\\\n');
-  fs.writeFile(path, wp.parseCommentsTemplate(dataToSave), (err) => {
+  fs.writeFile(path, parseCommentsTemplate(dataToSave), (err) => {
     if (err) {
       console.log(err); /* eslint-disable-line no-console */
     }
