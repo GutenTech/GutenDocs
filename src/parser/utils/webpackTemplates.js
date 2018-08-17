87976c6942d888939f44cd1ad69d4d1df8b0fb05
Uday Trivedi <trivediu@gmail.com>
2018-08-17 08:52:39 -0700

    Clean Up Webpack Templates

--- src/parser/utils/webpackTemplates.js
+++ src/parser/utils/webpackTemplates.js
@@ -1,22 +1,5 @@
 /*eslint-disable */
 
-// const parsedCommentsTemplate = `(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
-
-//   /***/ "./client/src/components/parsedData.json":
-//   /*!***********************************************!*\
-//     !*** ./client/src/components/parsedData.json ***!
-//     \***********************************************/
-//   /*! no static exports found */
-//   /***/ (function(module, exports, __webpack_require__) {
-  
-//   "use strict";
-//   eval(\`\n\nmodule.exports = ${dataToSave};\n\n//# sourceURL=webpack:///./client/src/components/parsedData.json?\`);
-  
-//   /***/ })
-  
-//   }]);`
-
-
 const parseCommentsTemplate = (dataToSave) => {
   return `(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
 
