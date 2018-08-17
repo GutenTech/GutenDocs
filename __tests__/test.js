6dcec8b68d1ffd536062ace7f5a97f02315d364c
gurkpet <gierkep@gmail.com>
2018-08-17 11:07:14 -0400

    travis please work

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
+const generateAPIFrame = require('./../src/generateAPIFrame.js');
 
 /* eslint-disable */
 const location = fs.readdirSync('./');
+console.log(location);
+generateAPIFrame('./', 'GutenApi');
+location = fs.readdirSync('./');
 console.log(location);
 it ('should be an example test', () => expect(true).toBe(true));
 
