fc86e7f3fe68a8071fa12eabbd60207ce494c39a
gurkpet <gierkep@gmail.com>
2018-08-18 19:46:37 -0400

    movedAllUtilsToUtils

--- src/refreshAPI.js
+++
@@ -1,9 +0,0 @@
-const { generateFilesaveArray } = require('./utils.js');
-const updateConfig = require('./updateConfig.js');
-
-const refreshAPI = (absPath, dirName) => {
-  generateFilesaveArray(absPath, dirName);
-  updateConfig(absPath.concat(dirName));
-};
-
-module.exports = refreshAPI;
--- src/utils.js
+++ src/utils.js
@@ -79,14 +79,30 @@ const copyFile = (absPath, destination, cb) => fs.readFile(absPath, (err, origin
   });
 });
 
-const replaceTheRCFile = (pathData) => {
+const findValidBackupName = (location, baseName) => {
+  const backupExt = path.extname(baseName);
+  let backupName = path.basename(baseName, backupExt).concat('.backup');
+  let filecount = 0;
+  if (fs.existsSync(location.concat(backupName).concat(backupExt))) {
+    while (fs.existsSync(location.concat(backupName).concat(filecount).concat(backupExt))) {
+      filecount += 1;
+    }
+    backupName = backupName.concat(filecount.toString());
+  }
+  return backupName.concat(backupExt);
+};
+
+const refreshFile = (pathData, fileName, source) => {
   /* eslint-disable-next-line no-console */
-  const RCFile = pathData.absPath.concat('.gutenrc.json');
+  const RCFile = pathData.absPath.concat(fileName);
   /* eslint-disable-next-line no-console */
-  console.log('Your .gutenrc.json file seems to no longer be a valid json file.');
-  const corruptJSONPrompt = [
+  console.log(`Your ${fileName} file seems to no longer be a valid file.`);
+  const corruptFilePrompt = [
     {
-      type: 'confirm', name: 'deleteRC', message: 'Can I erase the existing file and replace it with the default settings?', default: false,
+      type: 'confirm',
+      name: 'delete',
+      message: 'Can I erase the existing file and replace it with the default?',
+      default: false,
     },
   ];
   const confirmDeletePrompt = [
@@ -96,34 +112,101 @@ const replaceTheRCFile = (pathData) => {
       message: `You could lose the information currently in the file.
       Are you sure you want to overwrite it?`,
       choices: [
-        'I changed my mind, dont delete my .gutenrc.json',
-        'Save a copy as .gutenrc.backup',
+        `I changed my mind, dont delete my ${fileName}`,
+        `Save a copy as ${fileName}`,
         'Just overwrite it.',
       ],
     },
   ];
 
   inquirer
-    .prompt(corruptJSONPrompt)
+    .prompt(corruptFilePrompt)
     .then((answer) => {
-      if (answer.deleteRC === true) {
+      if (answer.delete === true) {
         inquirer
           .prompt(confirmDeletePrompt)
           .then((how) => {
-            if (how.method === 'I changed my mind, dont delete my .gutenrc.json') {
+            if (how.method === `I changed my mind, dont delete my ${fileName}`) {
               // do nothing
-            } else if (how.method === 'Save a copy as .gutenrc.backup') {
-              copyFile(RCFile, pathData.absPath.concat('.gutenrc.backup'), () => {
-                copyFile(pathData.absPath.concat('client/dist/.gutenRCTemplate.json'), RCFile);
+            } else if (how.method === `Save a copy as ${fileName}`) {
+              const backupName = findValidBackupName(pathData.absPath, fileName);
+              copyFile(RCFile, backupName, () => {
+                copyFile(pathData.absPath
+                  .concat(source), RCFile);
               });
             } else if (how.method === 'Just overwrite it.') {
-              copyFile(pathData.absPath.concat('client/dist/.gutenRCTemplate'), RCFile);
+              copyFile(pathData.absPath.concat(source), RCFile);
             }
           });
       }
     });
 };
 
-module.exports.replaceTheRCFile = replaceTheRCFile;
+const fillBlanksWithDefaults = (assignedSettings, defaultSettings) => {
+  const defaultConfig = fs.readFileSync(defaultSettings);
+  const mergedSettings = Object.assign(JSON.parse(assignedSettings), JSON.parse(defaultConfig));
+  return mergedSettings;
+};
+
+const updateConfig = (APIdir) => {
+  const pathToConfigBundle = APIdir.concat('1.bundle.js');
+  const pathToConfigJSON = APIdir.concat('gutenConfig.json');
+  if (!fs.existsSync(pathToConfigBundle)) {
+    /* eslint-disable-next-line no-console */
+    console.log(`Write Error:
+    The folder specified in the .gutenrc.json file seems to be missing.  
+    Update .gutenrc.json to match your API folder if you have changed the folder name,
+    or call "gutendocs --reset" if and only if you have accidentally deleted it 
+    and would like it to be reset to the original state.`);
+  } else {
+    let configSettings = fs.readFileSync(pathToConfigJSON);
+    const defaultFileLoc = path.dirname(__dirname).concat('/client/dist/gutenConfig.json');
+    configSettings = fillBlanksWithDefaults(configSettings, defaultFileLoc);
+
+    /* eslint-disable */
+    const fileToWrite = `(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+      /***/ "./client/src/components/configData.json":
+      /*!***********************************************!*\
+        !*** ./client/src/components/configData.json ***!
+        \***********************************************/
+      /*! no static exports found */
+      /***/ (function(module, exports, __webpack_require__) {
+      
+      "use strict";
+      eval(\`\n\nmodule.exports = ${JSON.stringify(configSettings)};\n\n//# sourceURL=webpack:///./client/src/components/configData.json?\`);
+      
+      /***/ })
+      
+      }]);`;
+      /* eslint-enable */
+    fs.writeFileSync(pathToConfigBundle, fileToWrite);
+  }
+};
+
+const refreshAPI = (absPath, dirName) => {
+  generateFilesaveArray(absPath, dirName);
+  updateConfig(absPath.concat(dirName));
+};
+
+const generateAPIFrame = (relPath, dirName) => {
+  const srcPath = path.dirname(__dirname).concat('/');
+  if (!fs.existsSync(relPath.concat('.gutenrc.json'))) {
+    const absPath = fs.realpathSync(relPath).concat('/');
+    generateFilesaveArray(absPath, dirName);
+    const templateRC = fs.readFileSync(srcPath.concat('client/dist/.gutenRCTemplate.json'));
+    const mergedRC = Object.assign(JSON.parse(templateRC), { apiDir: dirName });
+    fs.writeFileSync(absPath.concat('.gutenrc.json'), JSON.stringify(mergedRC));
+    updateConfig(absPath.concat(dirName));
+  } else {
+    /* eslint-disable-next-line no-console */
+    console.log('You have already initialized gutendocs in this Repo.  If you want to refresh the files call "gutendocs --reset"');
+  }
+};
+
+module.exports.generateAPIFrame = generateAPIFrame;
+module.exports.refreshAPI = refreshAPI;
+module.exports.refreshFile = refreshFile;
+module.exports.updateConfig = updateConfig;
 module.exports.generateFilesaveArray = generateFilesaveArray;
 module.exports.findRC = findRC;
