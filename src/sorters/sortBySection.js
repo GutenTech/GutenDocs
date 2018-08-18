087543d1f70213dfe468387fd9e7f9b9c2083ab3
Uday Trivedi <trivediu@gmail.com>
2018-08-18 12:17:51 -0700

    Implement CatchAll Priority Update and Headeer Update

--- src/sorters/sortBySection.js
+++ src/sorters/sortBySection.js
@@ -1,3 +1,15 @@
+/**
+ * @description A function that will assign the classification headings based upon
+ * whether or not end-user has specified a custom tag.  catchAllSection is option
+ * see params below.
+ * @param ast {[]} The AST with parsed information
+ * @param sectionTag {string} The matcher section tag to be searched for
+ * @param priority {number} The priority in which the sorting fxn was called in pipe
+ * @param catchAllSection {string} catchAllSection, if defined (and not a blank string)
+ * will file sections that are not labeled
+ */
+
+
 const sortBySection = (ast, sectionTag, priority, catchAllSection) => {
   const sectionName = sectionTag.slice(1);
   ast.forEach((file) => {
@@ -7,14 +19,19 @@ const sortBySection = (ast, sectionTag, priority, catchAllSection) => {
           /* eslint-disable */
           docBlock.header = tag.description;
           docBlock.priority = priority;
-        } else if (index === docBlock.tags - 1 && tag.title !== sectionName) {
-          // If section not detected, assign catchAll section (only if defined)
-          docBlock.header = catchAllSection === '' ? undefined : catchAllSection;
-          /* eslint-enable */
+        } else if (index === docBlock.tags.length - 1 && tag.title !== sectionName) {
+          // If section not detected yet, assign catchAll section and priority (only if defined)
+          if (docBlock.header === undefined && docBlock.priority === undefined) {
+            // increase priority by one to place catchAll Section at end, as no more sorts later 
+            docBlock.priority = catchAllSection === '' ? undefined : priority + 1;
+            docBlock.header = catchAllSection === '' ? undefined : catchAllSection;
+            /* eslint-enable */
+          }
         }
       });
     });
   });
+  console.log(JSON.stringify(ast));
   return ast;
 };
 
