2af09f0e3e0ca6e4b64fc6b44c62a5feae264118
Uday Trivedi <trivediu@gmail.com>
2018-08-18 14:08:13 -0700

    Add Errors in Sort By Section

---
+++ __tests__/sortBySection.test.js
@@ -0,0 +1,5 @@
+const { sortBySection } = require('../src/sorters/sortBySection.js');
+
+describe('sortBySection.js', () => {
+  it('should be a function', () => expect(sortBySection).toBeInstanceOf(Function));
+});
