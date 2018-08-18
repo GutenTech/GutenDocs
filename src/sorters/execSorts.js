414bc7e17fdca145d3a1c442ff0fd342821b1085
Uday Trivedi <trivediu@gmail.com>
2018-08-17 17:45:54 -0700

    Implement Reading of Array Into ExecSort File

--- src/sorters/execSorts.js
+++ src/sorters/execSorts.js
@@ -2,7 +2,8 @@ const gutenrc = require('../../.gutenrc.json');
 
 
 const execSorts = (ast) => {
-  console.log(gutenrc);
+  //console.log(gutenrc);
+  console.log(gutenrc.skeleton.sortByOrder);
 };
 
 module.exports.execSorts = execSorts;
