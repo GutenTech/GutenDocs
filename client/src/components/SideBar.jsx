f956739a34c9efafd658aa1b28537b1e98d1c46f
mcsteezy <davidpark9224@gmail.com>
2018-08-20 14:56:17 -0700

    sidebar and css

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -39,6 +39,57 @@ const SideBar = () => (
             <li>
              <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
             </li>
+                        <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+                        <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
+            <li>
+             <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+            </li>
         </ul>
 
     </nav>
@@ -46,6 +97,108 @@ const SideBar = () => (
 );
 
 export default SideBar;
+
+
+// class SideBar extends React.Component {
+//     construcutor(props) {
+//         super(props);
+//         this.state = {
+//           search: "level up"
+//         }
+//     };
+//     render() {
+//         return (
+//             <div className="wrapper">
+//                 <nav id="sidebar">
+//                     <div id="header">
+//                         <h5>GutenDocs</h5><br/>
+//                     </div>
+//                     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search..." ></input>
+//                     <ul className="list-unstyled components">
+//                         <li className="active">
+//                             <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Getting Started</a>
+//                             <ul className="collapse list-unstyled" id="homeSubmenu">
+//                                 <li>
+//                                     <a href="#">Installation</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">Examples</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">Support</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">FAQ</a>
+//                                 </li>
+//                             </ul>
+//                         </li>
+//                         <li>
+//                             <a href="#">Home</a>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#things'>Things</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                                     <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                                     <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//         )
+//     }
+// }
+
 
 
 // class SideBar extends React.Component {
