8d7213e8b537bc7183e5ec7b4ba417f50b249cab
gurkpet <gierkep@gmail.com>
2018-08-20 15:34:44 -0400

    added replace backticks method

--- src/parser/saveTags.js
+++ src/parser/saveTags.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { parseCommentsTemplate } = require('./utils/webpackTemplates.js');
 
 const saveTags = (data, path) => {
-  const dataToSave = JSON.stringify(data).replace(/\\n/g, '\\\\n');
+  const dataToSave = JSON.stringify(data).replace(/`/g, '\\`');
   fs.writeFile(path, parseCommentsTemplate(dataToSave), (err) => {
     if (err) {
       console.log(err); /* eslint-disable-line no-console */
