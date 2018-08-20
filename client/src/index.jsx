bd808287d45c6d708312779e924c41b545006a2a
gurkpet <gierkep@gmail.com>
2018-08-20 18:58:28 -0400

    clean up frontend lint errors

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 /*eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 /* eslint-enable */
 import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
