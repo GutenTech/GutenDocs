dea9c0a9fdd36d2be2261a238500dfee506f09f4
gurkpet <gierkep@gmail.com>
2018-08-20 17:32:04 -0400

    spacing issue for linter

--- src/parser/saveTags.js
+++ src/parser/saveTags.js
@@ -3,8 +3,8 @@ const { parseCommentsTemplate } = require('./utils/webpackTemplates.js');
 
 const saveTags = (data, path) => {
   const dataToSave = JSON.stringify(data)
-  .replace(/(?<!\\)(?:\\\\)*\\n/g, '\\\\n`') // replaces \n where there is odd number of \ with a \\n
-  .replace(/(?<!\\)(?:\\\\)*`/g, '\\`'); //  replaces ` and any ` preceded by an odd number of \ with a \`
+    .replace(/(?<!\\)(?:\\\\)*\\n/g, '\\\\n`') // replaces \n where there is odd number of \ with a \\n
+    .replace(/(?<!\\)(?:\\\\)*`/g, '\\`'); //  replaces ` and any ` preceded by an odd number of \ with a \`
   fs.writeFile(path, parseCommentsTemplate(dataToSave), (err) => {
     if (err) {
       console.log(err); /* eslint-disable-line no-console */
