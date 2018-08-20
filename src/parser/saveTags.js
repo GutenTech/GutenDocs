36bed29309252cad03c3e5c684a97e3652d9c430
Uday Trivedi <trivediu@gmail.com>
2018-08-20 12:03:01 -0700

    Working version of cleanup tags

--- src/parser/saveTags.js
+++ src/parser/saveTags.js
@@ -15,23 +15,27 @@ const cleanAST = (ast) => {
 
   ast.forEach((file) =>{
     file.content.forEach((commentBlock) => {
-      const updatedCommentBlock = {};
-      updatedCommentBlock.description = commentBlock.description;
-      updatedCommentBlock.tags = commentBlock.tags;
-      updatedCommentBlock.name = commentBlock.name;   
-      updatedCommentBlock.header = undefined;
-      updatedCommentBlock.priority = undefined;
-      // commentBlocks.push(updatedCommentBlock);
-      console.log(file.fileName);      
-      // commentBlocks.push({
-      //   description: commentBlock.description,
-      //   tags: commentBlock.tags,
-      //   name: commentBlock.name,
-      //   path: 
-      // });
+      // const updatedCommentBlock = {};
+      // updatedCommentBlock.description = commentBlock.description;
+      // updatedCommentBlock.tags = commentBlock.tags;
+      // updatedCommentBlock.name = commentBlock.name;   
+      // updatedCommentBlock.header = undefined;
+      // updatedCommentBlock.priority = undefined;  
+      commentBlocks.push({
+        header: undefined,
+        priority: undefined,
+        description: commentBlock.description,
+        tags: commentBlock.tags,
+        name: commentBlock.name,
+        pathName: file.fileName,
+      });
     });
   });
-
+  
+  for (let i = 0; i < commentBlocks.length; i++) {
+    console.log('*************************');
+    console.log(commentBlocks[i]);
+  }
   return commentBlocks;
 };
 
