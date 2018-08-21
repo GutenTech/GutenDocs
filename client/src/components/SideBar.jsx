ec8290af9cb791290cc402d0e25c7da3718bcb60
mcsteezy <davidpark9224@gmail.com>
2018-08-20 19:33:13 -0700

    webpage view fixed

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -52,3 +52,136 @@ const SideBar = () => (
 );
 
 export default SideBar;
+
+
+// class SideBar extends React.Component {
+//     construcutor(props) {
+//         super(props);
+//         this.state = {
+//           search: ""
+//         }
+//       }
+//       updateSearch(e) {
+//           this.setState({search: event.target.value.substr(0,25)});
+//     };
+//     render() {
+//         const parsedData = this.props.
+//         return (
+//             <div className="wrapper">
+//                 <nav id="sidebar">
+//                     <div id="header">
+//                         <h5>GutenDocs</h5><br/>
+//                     </div>
+//                     <input type="text" id="myInput" placeholder="Search..." onChange={this.updateSearch.bind(this)} ></input>
+//                     <ul className="list-unstyled components">
+//                         <li className="active">
+//                             {/* <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Getting Started</a> */}
+//                             <ul className="collapse list-unstyled" id="homeSubmenu">
+//                                 <li>
+//                                     <a href="#">Installation</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">Examples</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">Support</a>
+//                                 </li>
+//                                 <li>
+//                                     <a href="#">FAQ</a>
+//                                 </li>
+//                             </ul>
+//                         </li>
+//                         <li>
+//                             <a href="#">Home</a>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#things'>Things</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                                     <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                                     <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                         <li>
+//                         <AnchorLink offset={() => 100} href='#atName'>Name</AnchorLink>
+//                         </li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//         )
+//     }
+// }
+
+
+// class SideBar extends React.Component {
+//     constructor(){
+//       super();
+//       this.state ={
+//         parsedData: [],
+//         input: '',
+//       }
+//     }
+//     onChangeHandler(e){
+//       this.setState({
+//         input: e.target.value,
+//       })
+//     }
+//     render (){
+//         const list = this.state.parsedData
+//           .filter(d => this.state.input === '' || d.includes(this.state.input))
+//           .map((d, index) => <li key={index}>{d}</li>);
+//       return (<div>
+//         <input value={this.state.input} type="text" onChange={this.onChangeHandler.bind(this)}/>
+//           <ul>{list}</ul>
+//         </div>      
+//      );
+//     }
+//   }
+
+// export default SideBar;
