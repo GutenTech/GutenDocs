a5ed807d702b7b60e1dfa01f8dcf614cd041032e
Uday Trivedi <trivediu@gmail.com>
2018-08-21 20:44:27 -0700

    Update CLI.js File

--- bin/cli.js
+++ bin/cli.js
@@ -66,6 +66,7 @@ yargs.command('$0', 'Parse all file in dir and subdir', {
       const rawAST = parseComments(data, address);
       const ast = cleanAST(rawAST);
       const dataToWrite = execSorts(ast);
+      console.log(address);
       saveTags(dataToWrite, address);
     });
   }
