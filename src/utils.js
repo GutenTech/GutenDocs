9a0cfd71a542bf04889cd7b8f09148c37387bf34
gurkpet <gierkep@gmail.com>
2018-08-21 23:42:47 -0400

    update config on init

--- src/utils.js
+++ src/utils.js
@@ -262,7 +262,7 @@ const generateAPIFrame = (relPath, apiDir) => {
   } else {
     throw Error('You have already initialized gutendocs in this Repo.  If you want to refresh the files call "gutendocs --reset"');
   }
-  updateConfig({ absPath, apiDir });
+  updateConfig({ relPath, apiDir });
 };
 
 module.exports.generateAPIFrame = generateAPIFrame;
