668a43714b703b9492804783cd04ff44aab60c61
gurkpet <gierkep@gmail.com>
2018-08-22 20:56:07 -0400

    changed fillWIthBlank back to what it was while I figure it out

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,7 @@ const inquirerOptions = require('./inquirerOptions.js');
 
 const fillBlanksWithDefaults = (defaultSettingsPath, assignedSettings) => {
   const defaultConfig = fs.readFileSync(defaultSettingsPath);
-  const mergedSettings = Object.assign(JSON.parse(assignedSettings), JSON.parse(defaultConfig));
+  const mergedSettings = Object.assign(JSON.parse(defaultConfig, JSON.parse(assignedSettings)));
   return mergedSettings;
 };
 
