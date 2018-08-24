e2871dbc649b26ec1c8cb70ee33c21ab006fad9d
gurkpet <gierkep@gmail.com>
2018-08-24 01:29:06 -0400

    forgot to save

--- src/parser/saveTags.js
+++ src/parser/saveTags.js
@@ -13,7 +13,7 @@ const saveTags = (data, path) => {
     .replace(/(?<!\\)(?:\\\\)*\\n/g, '\\\\n') // replaces \n where there is odd number of \ with a \\n
     .replace(/(?<!\\)(?:\\\\)*`/g, '\\`') //  replaces ` and any ` preceded by an odd number of \ with a \`
     .replace(/(?<!\\)(?:\\)*\\"/g, '\\\\"') //  replaces \" and any \" preceded by an odd number of \ with a \\"
-    .replace(/\$\{/g, '\\$\\{'); //  replaces 
+    .replace(/\$\{/g, '\\$\\{'); //  replaces ${ with an escaped \$\{ so we dont have accidental string literals
   fs.writeFile(path, parseCommentsTemplate(dataToSave), (err) => {
     if (err) {
       console.log(err); /* eslint-disable-line no-console */
