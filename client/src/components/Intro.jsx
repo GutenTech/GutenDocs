5ce584ead9d58e894e0fd441a720d1e49d35fde2
gurkpet <gierkep@gmail.com>
2018-08-23 20:05:49 -0400

    now reads all keys from gutenconfig.json file

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -4,41 +4,25 @@ import PropTypes from 'prop-types';
 /* eslint-enable */
 // import example from '../../dist/imgs/example.png';
 // ideally we would have separate components for each h2
-const Intro = ({ text }) => (
-  <div className="intros">
-    <h2>Getting Started</h2>
-    <p>
-      {text.gettingStarted}
-    </p>
-    <br />
-    <br />
-    <h2>Installation</h2>
-    <p>
-      {text.installation}
-    </p>
-    <br />
-    <br />
-    <h2>Examples</h2>
-    <p>
-      {text.examples}
-    </p>
-    <br />
-    <br />
-    <h2>Support</h2>
-    <p>
-      {text.support}
-    </p>
-    <br />
-    <br />
-    <h2>FAQ</h2>
-    <p>
-      {text.FAQ}
-    </p>
-    <hr />
-    <br />
-    <br />
-  </div>
-);
+const Intro = ({ text }) => {
+  const headers = Object.keys(text);
+  return (
+    <div className="intros">
+      {
+        headers.map(header => (
+          <div key={header}>
+            <h2>
+              {header}
+            </h2>
+            <p>
+              {text[header]}
+            </p>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
 Intro.propTypes = {
   /* eslint-disable-next-line react/forbid-prop-types */
