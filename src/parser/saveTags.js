c927a775491cba44668dfd910ce13099670e7ed8
gurkpet <gierkep@gmail.com>
2018-08-24 01:27:53 -0400

    still working on successful parse of node_modules
    identified and fixed the problem where string literals were being evaluated in comments

--- src/parser/saveTags.js
+++ src/parser/saveTags.js
@@ -12,7 +12,8 @@ const saveTags = (data, path) => {
   const dataToSave = JSON.stringify(data)
     .replace(/(?<!\\)(?:\\\\)*\\n/g, '\\\\n') // replaces \n where there is odd number of \ with a \\n
     .replace(/(?<!\\)(?:\\\\)*`/g, '\\`') //  replaces ` and any ` preceded by an odd number of \ with a \`
-    .replace(/(?<!\\)(?:\\)*\\"/g, '\\\\"'); //  replaces \" and any \" preceded by an odd number of \ with a \\"
+    .replace(/(?<!\\)(?:\\)*\\"/g, '\\\\"') //  replaces \" and any \" preceded by an odd number of \ with a \\"
+    .replace(/\$\{/g, '\\$\\{'); //  replaces 
   fs.writeFile(path, parseCommentsTemplate(dataToSave), (err) => {
     if (err) {
       console.log(err); /* eslint-disable-line no-console */
