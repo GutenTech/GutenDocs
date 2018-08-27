d662470bc8b422acfe629b1b53581a089545bba9
gurkpet <gierkep@gmail.com>
2018-08-27 15:35:35 -0400

    more instructions added

--- client/dist/CustomSorters/sorterName.js
+++ client/dist/CustomSorters/sorterName.js
@@ -1,3 +1,22 @@
+// TODO use this file as a template.  You can copy and rename it whatever you want
+// TODO make sure you edit the .gutenrc file to define the custom options as well as
+// TODO the path and the function name
+
+/*
+! this is an example of what an alteredblock has available for you to define sort from
+  {
+    "description": "description from outside the tags array",
+    "tags": [
+      {
+        "title": "the type of tag it is (if it were a @param tag this would be "param")",
+        "description": "the description of the tag"
+      },
+    ],
+    "name": "the name of the function",
+    "pathName": "Path/to/your/file.js",
+  },
+*/
+
 // TODO add any modules you might need
 
 /**
