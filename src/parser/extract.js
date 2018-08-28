53db25b245057ddbcfe48d1df73bcb00cd8f756a
yuqi <zyqandrewda@gmail.com>
2018-08-28 14:00:09 -0400

    Extract bugfixs

--- src/parser/extract.js
+++ src/parser/extract.js
@@ -1,8 +1,18 @@
+<<<<<<< 595bbd8e39de9c3cd93d767089eed9c1dbcf4d95
 const acorn = require('acorn');
 const injectAcornJsx = require('acorn-jsx/inject');
 const injectAcornObjectRestSpread = require('acorn-object-rest-spread/inject');
+=======
+// const {
+//   parse,
+//   Parser,
+// } = require('acorn-jsx');
+const util = require('util')
+>>>>>>> Extract bugfixs
 const fs = require('fs');
 const path = require('path');
+const { parse, Parser } = require('acorn-object-rest-spread/inject')(require('acorn-jsx/inject')(require('acorn-stage3')));
+const walk = require("acorn/dist/walk")
 const {
   Walker,
 } = require('ignore-walk');
@@ -32,22 +42,33 @@ const exclude = (arr) => {
       }
       return `${r}\n!${relPath}/**/*.js\n!${relPath}/**/*.jsx`;
     }, '*');
-    walk.onReadIgnoreFile(1, rule, () => {});
+    walk.onReadIgnoreFile(1, rule, () => { });
     walk.start();
   });
 };
 
-const acornParse = (content, tagContent, gutenrc) => {
-  parse(content, {
+const acornParse = (content, tagContent) => {
+  const arr = [];
+  const tree = parse(content, {
     plugins: {
       jsx: true,
+<<<<<<< 595bbd8e39de9c3cd93d767089eed9c1dbcf4d95
+=======
+      stage3: true,
+>>>>>>> Extract bugfixs
       objectRestSpread: true,
     },
-    ...gutenrc.acornSettings,
+    ecmaVersion: 10,
+    allowReserved: true,
+    allowReturnOutsideFunction: true,
+    allowImportExportEverywhere: true,
+    allowAwaitOutsideFunction: true,
+    allowHashBang: true,
     onComment: (b, t, s, d) => {
       if (b && t[0] === '*') {
-        const a = {
+        arr.push({
           comment: t,
+<<<<<<< 595bbd8e39de9c3cd93d767089eed9c1dbcf4d95
         };
         const p = new Parser({
           plugins: {
@@ -59,10 +80,92 @@ const acornParse = (content, tagContent, gutenrc) => {
         const node = p.parseStatement(true, true, exports);
         a.name = node.id ? node.id.name : node.declarations[0].id.name;
         tagContent.push(a);
+=======
+          pos: d,
+        });
+>>>>>>> Extract bugfixs
       }
     },
   });
+  // console.log('arr', arr);
+  arr.forEach((x) => {
+    const node = walk.findNodeAfter(tree, x.pos).node;
+    // console.log('node', node.type);
+    switch (node.type) {
+      case 'MethodDefinition':
+        tagContent.push({
+          comment: x.comment,
+          name: node.key.name,
+        });
+        return;
+      case 'VariableDeclaration':
+        node.declarations.forEach(
+          declaration => tagContent.push(
+            {
+              comment: x.comment,
+              name: declaration.id.name,
+            },
+          ),
+        );
+        return;
+      case 'FunctionDeclaration':
+        tagContent.push({
+          comment: x.comment,
+          name: node.id.name,
+        });
+        return;
+      default:
+    }
+  }, []);
 };
+// const acornParse = (content, tagContent, gutenrc) => {
+//   const type = ['ClassDeclaration', 'FunctionDeclaration', 'VariableDeclaration'];
+//   parse(content, {
+//     plugins: {
+//       jsx: true,
+//       // stage3: true,
+//       // objectRestSpread: true,
+//     },
+//     ecmaVersion: 10,
+//     // allowReserved: true,
+//     allowReturnOutsideFunction: true,
+//     allowImportExportEverywhere: true,
+//     // allowAwaitOutsideFunction: true,
+//     allowHashBang: true,
+//     locations: true,
+//     onComment: (b, t, s, d, l) => {
+//       if (b && t[0] === '*') {
+//         const p = new Parser({
+//           plugins: {
+//             jsx: true,
+//             stage3: true,
+//             objectRestSpread: true,
+//           },
+//           ecmaVersion: 10,
+//           allowReserved: true,
+//           allowReturnOutsideFunction: true,
+//           allowImportExportEverywhere: true,
+//           allowAwaitOutsideFunction: true,
+//           allowHashBang: true,
+//         }, content, d);
+//         p.nextToken();
+//         const node = p.parseStatement(true, true);
+//         // console.log('node', node);
+//         if (type.includes(node.type)) {
+//           // console.log('location', l);
+//           // console.log('node', node);
+//           const a = {
+//             comment: t,
+//           };
+//           // console.log(node);
+//           // console.log(util.inspect(myObject, false, null))
+//           a.name = node.id ? node.id.name : node.declarations[0].id.name;
+//           tagContent.push(a);
+//         }
+//       }
+//     },
+//   });
+// };
 
 const extract = arr => exclude(arr).then((list) => {
   const gutenrc = getRC();
@@ -88,6 +191,7 @@ const extract = arr => exclude(arr).then((list) => {
         console.log(`\n\nError processing ${file}\n**********\n${e.message}\n**********\n`);
       }
     }
+    // console.log('content', tag.content);
     if (gutenrc.verbosity >= 2) {
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
@@ -112,4 +216,5 @@ const extract = arr => exclude(arr).then((list) => {
   return new Promise(resolve => resolve(result));
 });
 
+extract(['parseComments.js'])
 module.exports = extract;
